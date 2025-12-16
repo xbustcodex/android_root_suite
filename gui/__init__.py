94402e673eb3980c9a2f09e88dad96b90d886312
adzs <xkali6x@gmail.com>
2025-12-16 14:15:12 +1100

    Add docstring and update exports in __init__.py
    Added module docstring and updated __all__ exports.

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,1 +1,9 @@
+"""
+GUI module for Android Root Suite
+"""
 
+from .app import ADBRootToolGUI
+from .styles import StyleManager
+from .utils import DialogHelper, ThreadHelper
+
+__all__ = ['ADBRootToolGUI', 'StyleManager', 'DialogHelper', 'ThreadHelper']
