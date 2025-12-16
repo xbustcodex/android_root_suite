f521ca391dea49e919a5ae1ab3aa4154ea58034c
adzs <xkali6x@gmail.com>
2025-12-16 14:48:27 +1100

    Add brand-specific tools to __init__.py

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,1 +1,10 @@
+"""
+Brand-specific tools modules
+"""
 
+from .samsung_tools import SamsungTools
+from .xiaomi_tools import XiaomiTools
+from .qualcomm_tools import QualcommTools
+from .mediatek_tools import MediaTekTools
+
+__all__ = ['SamsungTools', 'XiaomiTools', 'QualcommTools', 'MediaTekTools']
