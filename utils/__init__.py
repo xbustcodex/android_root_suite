2173689bd36c5651efecbd3547e43ea81a6128ee
adzs <xkali6x@gmail.com>
2025-12-16 14:51:51 +1100

    Enhance utils module with new functions
    Added utility functions and logging setup to the module.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,22 @@
+"""
+Utility functions module
+"""
 
+from .file_utils import (
+    get_directory_size,
+    format_file_size,
+    cleanup_old_backups,
+    find_files_by_extension
+)
+from .thread_utils import run_in_thread
+from .logging_utils import setup_logging, get_logger
+
+__all__ = [
+    'get_directory_size',
+    'format_file_size',
+    'cleanup_old_backups',
+    'find_files_by_extension',
+    'run_in_thread',
+    'setup_logging',
+    'get_logger'
+]
