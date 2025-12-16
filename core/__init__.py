f1b17ecff41988d6df3e2913a6e47fe7ee184bad
adzs <xkali6x@gmail.com>
2025-12-16 14:11:42 +1100

    Add module docstring and import statements

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,10 @@
+"""
+Core functionality modules
+"""
 
+from .adb_manager import ADBManager
+from .device_manager import DeviceManager
+from .backup_manager import BackupManager
+from .file_manager import FileManager
+
+__all__ = ['ADBManager', 'DeviceManager', 'BackupManager', 'FileManager']
